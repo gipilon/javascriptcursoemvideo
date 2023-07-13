a93d022aa41e0710d4717d3e739587ba322d544c
gipilon <115512436+gipilon@users.noreply.github.com>
2023-07-13 17:34:22 -0300

    Add files via upload
    javscript

---
+++ iniciando javascript/curso.js/ex1/aula 11/008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log('A velocidade do seu carro é ' + vel)
+if (vel > 50) {
+    console.log('VOcê ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
